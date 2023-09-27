dcd59431e4c46ef227e403e410ff8fce2154af72
Crazelu <eberelucky64@gmail.com>
2023-09-27 07:02:01 +0100

    Animates estimated amount

--- lib/presentation/views/calculate/calculation_result_view.dart
+++ lib/presentation/views/calculate/calculation_result_view.dart
@@ -1,9 +1,47 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:movemate/presentation/resources/assets.dart';
 import 'package:movemate/presentation/widgets/custom_text.dart';
 
-class CalculationResultView extends StatelessWidget {
+class CalculationResultView extends StatefulWidget {
   const CalculationResultView({super.key});
+
+  @override
+  State<CalculationResultView> createState() => _CalculationResultViewState();
+}
+
+class _CalculationResultViewState extends State<CalculationResultView> {
+  Timer? _timer;
+
+  static const int _max = 1460;
+  int _amount = 0;
+
+  void _animateAmount() {
+    _timer?.cancel();
+    _timer = Timer.periodic(
+      const Duration(milliseconds: 2),
+      (timer) {
+        setState(() {
+          _amount += 5;
+        });
+        if (_amount == _max) {
+          timer.cancel();
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(_animateAmount);
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +72,11 @@ class CalculationResultView extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 8),
-          const CustomText(
-            text: "\$1460 USD",
+          CustomText(
+            text: "\$$_amount USD",
             fontSize: 18,
             textAlign: TextAlign.center,
-            color: Color(0xff4aca2d),
+            color: const Color(0xff4aca2d),
           ),
           const SizedBox(height: 8),
           const CustomText(
